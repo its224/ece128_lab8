408d9f758899c6ca504c0cfe1cea8469d5151e17
its224 <111983184+its224@users.noreply.github.com>
2023-10-31 15:20:51 -0400

    Add files via upload

---
+++ refresh_counter.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/31/2023 01:58:40 PM
+// Design Name: 
+// Module Name: refresh_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module refresh_counter(
+    input clk, 
+    output reg [1:0] cnt_q = 0
+    );
+    reg[1:0] cnt_d = 0;
+    
+    always @(posedge clk)
+    begin
+        if(cnt_d == 2'b11)
+            cnt_d = 2'b0;
+        else
+            cnt_d = cnt_d + 1'b1;
+            cnt_q <= cnt_d;
+    end
+endmodule
